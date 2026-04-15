0f82561bddd43694fe16769d8876466e2fe8923b
baracinha <francisco.baracinha@gmail.com>
2026-04-15 17:47:59 +0100

    tipos

--- melonmessage/js/auth.js
+++ melonmessage/js/auth.js
@@ -1,3 +1,4 @@
+0
 import { posts } from './calls.js';
 
 export async function Login(e){
@@ -5,7 +6,10 @@ export async function Login(e){
         const username = document.getElementById('name').value;
         const password = document.getElementById('password').value;
         
-        const data = await posts('/login', {username, password});
+        const data = await posts('/login', {
+            username: username,
+            password_hash: password,
+        });
             if (data.nomeUser) {
                 localStorage.setItem('nomeUser', data.nomeUser);
                 localStorage.setItem('idUser', data.idUser);
@@ -19,15 +23,20 @@ export async function criarconta(e){
     e.preventDefault();
 
         const name = document.getElementById('name').value;
+        const cidade = document.getElementById('cidade').value
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
         const tel = document.getElementById('tel').value;
 
         const data = await posts('/register', {
-            username: name,
+            nome: name,
             email: email,
-            telefone: tel,
-            password: password,
+            telefone: Number(tel),
+            password_hash: password,
+            cargo: 'admin',
+            bio: "",
+            imagem_perfil: "",
+            cidade: cidade,
         }); 
             if(data.id){
                 console.log('conta criada com sucesso');
